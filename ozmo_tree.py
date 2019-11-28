e6aa4f260b21f24f1434a05317f443aa1c32384b
Kyle Krcmaric <krcm0209@gmail.com>
2019-11-27 20:52:58 -0500

    untested, blind-coded script for controlling tree lighting

--- ozmo_tree.py
+++ ozmo_tree.py
@@ -4,7 +4,7 @@ import os
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 9)
+pixels = neopixel.NeoPixel(board.D18, 288, auto_write=False)
 
 adc = MCP3001()
 strobe = DigitalOutputDevice(2)
@@ -23,22 +23,54 @@ def readAmplitude():
     
     return readADC()
 
+def createTreeObject():
+    tri = list([0, 0, 0])
+    tree = list()
+    
+    tree.append([                    tri                    ])
+    
+    tree.append([                tri,tri,tri                ])
+
+    tree.append([            tri,tri,tri,tri,tri            ])
+
+    tree.append([                tri,tri,tri                ])
+
+    tree.append([            tri,tri,tri,tri,tri            ])
+
+    tree.append([        tri,tri,tri,tri,tri,tri,tri        ])
+
+    tree.append([            tri,tri,tri,tri,tri            ])
+
+    tree.append([        tri,tri,tri,tri,tri,tri,tri        ])
+
+    tree.append([    tri,tri,tri,tri,tri,tri,tri,tri,tri    ])
+
+    tree.append([        tri,tri,tri,tri,tri,tri,tri        ])
+
+    tree.append([    tri,tri,tri,tri,tri,tri,tri,tri,tri    ])
+
+    tree.append([tri,tri,tri,tri,tri,tri,tri,tri,tri,tri,tri])
+
+    tree.reverse()
+    return tree
+
 def run():
     minReading = 0.0001
     maxReading = 0.1
     readRange = None
-    uniqueReadValues = set()
     
-    ROWS = 10
-    COLUMNS = 7
-    visual = [[0]*COLUMNS]*ROWS
+    FREQ_BANDS = 7
+    BAND_THRESHOLDS = (1/7, 2/7, 3/7, 4/7, 5/7, 6/7, 7/7)
+    NUM_LEDS = 288
+
+    spectrum = [0] * FREQ_BANDS
+    tree = createTreeObject()
 
     reset.off()
     while True:
         freqAmplitudes = list()
-        for i in range(COLUMNS):
+        for band in range(FREQ_BANDS):
             value = readAmplitude()
-            # print('{} = {}'.format(i, value))
             freqAmplitudes.append(readAmplitude())
         
             if minReading is None or minReading > value:
@@ -48,9 +80,31 @@ def run():
             
             readRange = maxReading - minReading
             mapRange = 255
-            neoValue = int(((value - minReading) * mapRange) / readRange)
-            pixels[i] = (neoValue, 0, 0)
-            # uniqueReadValues.add(value)
-        # 
-        # print('Read range = ', readRange)
-        # print('Unique read values = ', len(uniqueReadValues))
+            spectrum[band] = int(((value - minReading) * mapRange) / readRange)
+
+        for i, row in enumerate(tree):
+            height = i/len(tree)
+            ratio = len(row)/FREQ_BANDS
+
+            pixel = 0
+
+            if i % 2:
+                tree[row].reverse()
+            
+            for j, triangle in enumerate(row):
+                amplitude = spectrum[int(round((j/len(row))*FREQ_BANDS))]
+                if amplitude > 0.1:
+                    for i in range(4):
+                        pixels[pixel] = (amplitude, 0, 0)
+                        pixel += 1
+                    # tree[row][triangle] = [amplitude, 0, 0]
+                else:
+                    for i in range(4):
+                        pixels[pixel] = (0, 127, 0)
+                        pixel += 1
+                    # tree[row][triangle] = [0, 127, 0]
+
+            if i % 2:
+                tree[row].reverse()
+
+        pixels.show()
